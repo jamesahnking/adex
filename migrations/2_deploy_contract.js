6c5473684239c89df5bdb8c1bc6522859356aa63
jamesahnking <jamesahnking@users.noreply.github.com>
2022-03-08 13:08:18 -0500

    - Made change making swiching addToken to a Promise.all

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -26,17 +26,17 @@ module.exports = async function(deployer, _network, accounts) {
         [Dai, Bat, Ada, Smt, Dex].map(contract => contract.deployed())
     );  
     
-    await dex.addToken(DAI, dai.address);
-    await dex.addToken(BAT, bat.address);
-    await dex.addToken(ADA, ada.address);
-    await dex.addToken(SMT, smt.address);
+    // await dex.addToken(DAI, dai.address);
+    // await dex.addToken(BAT, bat.address);
+    // await dex.addToken(ADA, ada.address);
+    // await dex.addToken(SMT, smt.address);
     
-    // await Promise.all([
-    //     await dex.addToken(DAI, dai.address),
-    //     await dex.addToken(BAT, bat.address),
-    //     await dex.addToken(ADA, ada.address),
-    //     await dex.addToken(SMT, smt.address)
-    // ]);    
+    await Promise.all([
+        await dex.addToken(DAI, dai.address),
+        await dex.addToken(BAT, bat.address),
+        await dex.addToken(ADA, ada.address),
+        await dex.addToken(SMT, smt.address)
+    ]);    
 
     // define amount of token allocation '1000' wei
     const amount = web3.utils.toWei('1000');
@@ -104,64 +104,66 @@ const increaseTime = async (seconds) => {
     }, () => {});
   }
 
-// create trades
+// Trades 
 await dex.createLimitOrder(1000, BAT, 10, SIDE.BUY, {from: trader1});
 await dex.createMarketOrder(1000, BAT, SIDE.SELL, {from: trader2});
 await increaseTime(1);
 
-// await dex.createLimitOrder(BAT, 1200, 11, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(BAT, 1200, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder(1200, BAT, 11, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(1200,BAT, SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(BAT, 1200, 15, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(BAT, 1200, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder(1200, BAT, 15, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(1200, BAT, SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(BAT, 1500, 14, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(BAT, 1500, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder(1500, BAT, 14, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(1500, BAT,  SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(BAT, 2000, 12, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(BAT, 2000, SIDE.SELL, {from: trader2});
+await dex.createLimitOrder( 2000, BAT,12, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder( 2000,BAT, SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(ADA, 1000, 2, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(ADA, 1000, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder(1000, ADA, 2, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(1000, ADA, SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(ADA, 500, 4, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(ADA, 500, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder(500, ADA, 4, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(500, ADA,  SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(ADA, 800, 2, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(ADA, 800, SIDE.SELL, {from: trader2});
-// await increaseTime(1);
+await dex.createLimitOrder( 800, ADA, 2, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder( 800, ADA, SIDE.SELL, {from: trader2});
+await increaseTime(1);
 
-// await dex.createLimitOrder(ADA, 1200, 6, SIDE.BUY, {from: trader1});
-// await dex.createMarketOrder(ADA, 1200, SIDE.SELL, {from: trader2});
+await dex.createLimitOrder( 1200, ADA, 6, SIDE.BUY, {from: trader1});
+await dex.createMarketOrder(1200, ADA, SIDE.SELL, {from: trader2});
 
+// Orders
 
-// await dex.createLimitOrder(BAT, 1400, 10, SIDE.BUY, {from: trader1});
-// await dex.createLimitOrder(BAT, 1200, 11, SIDE.BUY, {from: trader2});
-// await dex.createLimitOrder(BAT, 1000, 12, SIDE.BUY, {from: trader2});
+await dex.createLimitOrder(1400, BAT, 10, SIDE.BUY, {from: trader1});
+await dex.createLimitOrder(1200, BAT, 11, SIDE.BUY, {from: trader2});
+await dex.createLimitOrder(1000, BAT, 12, SIDE.BUY, {from: trader2});
 
-// await dex.createLimitOrder(ADA, 3000, 4, SIDE.BUY, {from: trader1});
-// await dex.createLimitOrder(ADA, 2000, 5, SIDE.BUY, {from: trader1});
-// await dex.createLimitOrder(ADA, 500, 6, SIDE.BUY, {from: trader2});
+await dex.createLimitOrder(3000, ADA, 4, SIDE.BUY, {from: trader1});
+await dex.createLimitOrder(2000, ADA, 5, SIDE.BUY, {from: trader1});
+await dex.createLimitOrder(500, ADA, 6, SIDE.BUY, {from: trader2});
 
-// await dex.createLimitOrder(SMT, 4000, 12, SIDE.BUY, {from: trader1});
-// await dex.createLimitOrder(SMT, 3000, 13, SIDE.BUY, {from: trader1});
-// await dex.createLimitOrder(SMT, 500, 14, SIDE.BUY, {from: trader2});
+await dex.createLimitOrder(4000, SMT, 12, SIDE.BUY, {from: trader1});
+await dex.createLimitOrder(3000, SMT, 13, SIDE.BUY, {from: trader1});
+await dex.createLimitOrder(500, SMT, 14, SIDE.BUY, {from: trader2});
 
-// await dex.createLimitOrder(BAT, 2000, 16, SIDE.SELL, {from: trader3});
-// await dex.createLimitOrder(BAT, 3000, 15, SIDE.SELL, {from: trader4});
-// await dex.createLimitOrder(BAT, 500, 14, SIDE.SELL, {from: trader4});
+await dex.createLimitOrder(2000, BAT, 16, SIDE.SELL, {from: trader3});
+await dex.createLimitOrder(3000, BAT, 15, SIDE.SELL, {from: trader4});
+await dex.createLimitOrder(500, BAT, 14, SIDE.SELL, {from: trader4});
 
-// await dex.createLimitOrder(ADA, 4000, 10, SIDE.SELL, {from: trader3});
-// await dex.createLimitOrder(ADA, 2000, 9, SIDE.SELL, {from: trader3});
-// await dex.createLimitOrder(ADA, 800, 8, SIDE.SELL, {from: trader4});
+await dex.createLimitOrder(4000, ADA, 10, SIDE.SELL, {from: trader3});
+await dex.createLimitOrder(2000,ADA,  9, SIDE.SELL, {from: trader3});
+await dex.createLimitOrder(800, ADA, 8, SIDE.SELL, {from: trader4});
 
-// await dex.createLimitOrder(SMT, 1500, 23, SIDE.SELL, {from: trader3});
-// await dex.createLimitOrder(SMT, 1200, 22, SIDE.SELL, {from: trader3});
-// await dex.createLimitOrder(SMT, 900, 21, SIDE.SELL, {from: trader4});
+await dex.createLimitOrder(1500, SMT, 23, SIDE.SELL, {from: trader3});
+await dex.createLimitOrder(1200, SMT,  22, SIDE.SELL, {from: trader3});
+await dex.createLimitOrder(900, SMT, 21, SIDE.SELL, {from: trader4});
 
 };
